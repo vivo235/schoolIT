844bc7046dedeaacf6058431ed25f0dede5516be
vivo235 <lfka1834@gmail.com>
2025-06-01 16:14:38 +0300

    Add files via upload

---
+++ hw10/script.js
@@ -0,0 +1,21 @@
+function isGreater(){
+    let one = 30;
+    let two = 20;
+    if(one > two){
+        console.log("перше число більше за друге");
+    }else{
+        console.log("Друге число більше за перше");
+    }
+}
+isGreater()
+
+function areEqual(){
+    let one = 30;
+    let two = 10;
+    if(one == two){
+        console.log("числа рівні");
+    }else{
+        console.log("числа різні");
+    }
+}
+areEqual();
